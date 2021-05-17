82b1f503d14e782db8aec0edd69805b288b5a2da
Rishabh200729 <duttrishabh26@gmail.com>
2021-05-17 15:21:40 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/firestore"; 
 //options and config info
+console.log(process.env);
+
 const firebaseConfig = {
-  apiKey: process.env.React_App_API_KEY,
-  authDomain: process.env.React_App_AUTH_DOMAIN,
-  projectId: process.env.React_App_PROJECT_ID,
-  storageBucket:process.env.React_App_STORAGE_BUCKET ,
-  messagingSenderId: process.env.React_App_MESSAGE_SENDER_ID,
-  appId: process.env.React_App_APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 //init app
 firebase.initializeApp(firebaseConfig);
